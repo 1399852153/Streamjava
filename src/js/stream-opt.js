9fb3878b7bd46debccf723c3feb0cb7bcbeea5c0
1399852153 <139985215.@qq.com>
2019-03-03 23:32:51 +0800

    初始化

--- src/js/stream-opt.js
+++ src/js/stream-opt.js
@@ -44,35 +44,32 @@ function filter_stream(predicate,stream){
 
 // stream accumulate 操作
 function accumulate_stream(combiner,initVal,stream){
-	debugger;
 	if(is_empty_stream(stream)){
 		return initVal;
 	}
 	
-	return combiner(
-		head(stream),
-		accumulate_stream(combiner,initVal,tail(stream))
-	);
+	let v1 = head(stream);
+	let v2 = accumulate_stream(combiner,initVal,tail(stream));
+	
+	return combiner(v1,v2);
 }
 
 // stream append 操作
 // 在stream1后面拼接stream2
 function append_stream(stream1,stream2){
-	debugger;
 	if(is_empty_stream(stream1)){
 		return stream2;
 	}
 	
-	return cons_stream(
-		head(stream1),
-		append_stream(tail(stream1),stream2)
-	);
+	let the_head = head(stream1);
+	let the_tail = append_stream(tail(stream1),stream2);
+	
+	return cons_stream(the_head,the_tail);
 }
 
 // stream flatten 扁平化操作
 function flatten_stream(stream_in_stream){
-	debugger;
-	accumulate_stream(
+	return accumulate_stream(
 		append_stream,
 		empty_stream(),
 		stream_in_stream
